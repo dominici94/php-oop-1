097333deaa77a67d9680de20f1d38631851a43f5
dominici94 <mattiadominici94@gmail.com>
2022-01-28 17:39:12 +0100

    esercizio

--- classes/Movie.php
+++ classes/Movie.php
@@ -1,15 +1,69 @@
 <?php
 
 class Movie {
-    public $titolo;
-    public $regista;
-    public $durata;
+    private $titolo;
+    private $regista;
+    private $durata;
     public $genere;
-    public $anno;
+    private $anno;
 
-    public function datiFilm(){
-        echo "{$this->titolo} {$this->regista} {$this->durata} {$this->anno}";
+    // costruttore (che viene lanciato alla costruzione dell'oggetto) definisce argomenti obbligatori in fase di costruzione dell'oggetto
+
+    public function __construct($_titolo, $_regista, $_durata, $_anno)
+    {
+        $this->setTitolo($_titolo);
+        $this->regista = $_regista;
+        $this->durata = $_durata;
+        $this->anno = $_anno;
     }
+
+    public function setTitolo($_titolo)
+    {
+        // FORMA DI SICUREZZA per impedire di impostare un titolo < 3 caratteri per esempio e avere maggiore controllo
+        if(strlen($_titolo) > 3){
+            $this->titolo = $_titolo;
+        }
+    }
+
+    public function getTitolo()
+    {
+        return $this->titolo;
+    }
+
+    public function setRegista($_regista)
+    {
+        $this->regista = $_regista;
+    }
+
+    public function getRegista()
+    {
+        return $this->regista;
+    }
+
+    public function setDurata($_durata)
+    {
+        $this->durata = $_durata;
+    }
+
+    public function getDurata()
+    {
+        return $this->durata;
+    }
+
+    public function setAnno($_anno)
+    {
+        $this->anno = $_anno;
+    }
+
+    public function getAnno()
+    {
+        return $this->anno;
+    }
+
+    // public function getDatiFilm()
+    // {
+    //     return "{$this->titolo} {$this->regista} {$this->durata} {$this->anno}";
+    // }
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,22 +1,39 @@
 <?php
 require_once __DIR__ . '/classes/Movie.php';
 
-$invictus = new Movie;
-
-$invictus->titolo = "Invictus";
-$invictus->regista = "Clint Eastwood";
-$invictus->durata = 134;
-$invictus->genere = ["drammatico", "sportivo", "biografico"];
-$invictus->anno = 2009;
-
-$batman = new Movie;
-
-$superman = new Movie;
-
-$spiderman = new Movie;
+$movies = [
+    $invictus = new Movie("Invictus - L'invincibile", "Clint Eastwood", 134, 2009),
+    $batman = new Movie("Batman Begins", "Cristopher Nolan", 140, 2005),
+    $superman = new Movie("Superman returns", "Bryan Singer", 154, 2006),
+    $spiderman = new Movie("Spider-Man", "Sam Raimi", 121, 2002)
+];
 
 
-$invictus->datiFilm();
+// con titolo public:
+// $invictus->titolo = "Invictus - L'invincibile";
+
+// con titolo private:
+// $invictus->setTitolo("Invictus - L'invincibile pirupiru");
+
+// $invictus->setRegista("Clint Eastwood pirupiru");
+// $invictus->setDurata(134);
+// $invictus->genere = ["drammatico", "sportivo", "biografico"];
+// $invictus->setAnno(2009);
+
+
+// $batman->setDurata(140);
+// $batman->setAnno(2005);
+
+
+// $superman->setDurata(154);
+// $superman->setAnno(2006);
+
+
+// $spiderman->setDurata(121);
+// $spiderman->setAnno(2002);
+
+
+// echo "<h1>Titolo: {$invictus->getDatiFilm()}</h1>";
 
 ?>
 
@@ -37,6 +54,16 @@ all'interno della classe sono dichiarate delle variabili d'istanza
 all'interno della classe è definito un costruttore
 all'interno della classe è definito almeno un metodo
 vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
+    <h1>Movies</h1>
+    <?php foreach($movies as $movie) { ?>
+        <div>
+            <h2><?php echo "Titolo: {$movie->getTitolo()}" ?></h2>
+            <h3><?php echo "Regista: {$movie->getRegista()}" ?></h3>
+            <h3><?php echo "Durata: {$movie->getDurata()} minuti" ?></h3>
+            <h3><?php echo "Anno: {$movie->getAnno()}" ?></h3>
+            <hr>
+        </div>
+    <?php } ?>
 
 
 </body>
